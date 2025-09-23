af2493392e8960329b093dce7c843c40ac3755bd
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-09-23 22:49:31 +0530

    Updated with functional programming

--- lab-internal-1/p2_BitwiseOperations.java
+++ lab-internal-1/p2_BitwiseOperations.java
@@ -1,25 +1,41 @@
-// 2.Write a java program that contains a string (char pointer) with a value 'Hello World'. The 
-// program should AND or and XOR each character in this string with 127 and displays the result. 
-// Program: 
+/* 2.Write a java program that contains a string (char pointer) with a value 'Hello World'. The 
+program should AND , OR and XOR each character in this string with 127(01111111) and displays the result. 
+*/
 
+import java.util.function.*;
 public class p2_BitwiseOperations{
+    
+    // takes two characters and returns a character
+    private static final BinaryOperator<Character> calc = (ch,op) ->{
+        return (char) switch (op) {
+            case '&'-> (ch & 127);
+            case '|'-> (ch | 127);
+            case '^'-> (ch ^ 127);
+            default -> ch;
+        };
+    };
 
-    public static void main(String[] args) {
+    // A Bi-function which takes a string and operator 
+    private static final BiFunction<String,Character,String> compute = (str,op) -> {
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : str.toCharArray()){
+            sb.append(calc.apply(ch, op));
+        }
+        return sb.toString();
+    };
+    public static void main(String... bitwise) {
+        
         String text = "Hello World";
         System.out.println("Original String: " + text);
-        // Perform AND operation with 127 and display the result System.out.print("AND with 127: ");  
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            char andResult = (char) (c & 127);
-            System.out.print(andResult);
-        }
-        System.out.println();
-        // Perform XOR operation with 127 and display the result System.out.print("XOR with 127: "); 
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            char xorResult = (char) (c ^ 127);
-            System.out.print(xorResult);
-        }
-        System.out.println();
+        
+        // Perform AND operation with 127 and display the result 
+        System.out.println("AND with 127: " + compute.apply(text, '&'));
+        
+        // Perform OR operation with 127 and display the result 
+        System.out.println("OR with 127: " + compute.apply(text, '|'));
+        
+        // Perform XOR operation with 127 and display the result 
+        System.out.println("XOR with 127: " + compute.apply(text, '^'));
     }
 }
