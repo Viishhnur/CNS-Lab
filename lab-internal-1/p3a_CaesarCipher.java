af0759a82196cec73bb848ad40bd30dfc085e925
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-09-23 23:33:14 +0530

    Updated encryption and decryption methods

--- lab-internal-1/p3a_CaesarCipher.java
+++ lab-internal-1/p3a_CaesarCipher.java
@@ -1,41 +1,73 @@
-// 3. A)Write a java program to perform encryption and decryption using the Ceaser cipher 
-// algorithm? 
-// Program: 
+// 3A) Write a java program to perform encryption and decryption using the Ceaser cipher algorithm
 
 import java.util.Scanner;
-
+import java.util.function.*;
 public class p3a_CaesarCipher {
 
-    public static String encrypt(String message, int shift) {
-    StringBuilder result = new StringBuilder();
-    for (char ch : message.toCharArray()) {
-        if (Character.isUpperCase(ch))
-            ch = (char) ((ch - 'A' + shift) % 26 + 'A');
-        else if (Character.isLowerCase(ch))
-            ch = (char) ((ch - 'a' + shift) % 26 + 'a');
-        result.append(ch);
-    }
-    return result.toString();
-}
+    // Predicates to find isUpper or isLower
+    private static final Predicate<Character> isLower = ch -> Character.isLowerCase(ch);
+    private static final Predicate<Character> isUpper = Character :: isUpperCase; // this is method reference
 
-    public static String decrypt(String message, int shift) {
-        return encrypt(message, 26 - shift); // Decrypt is reverse of encrypt with 26 - shift 
-    }
+    private static final BiFunction<String,Integer,String> encrypt = (msg,shift) -> {
+        // In ceaser cyper we should shift each character to next kth character(here k = shift)
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the message: ");
-        String message = scanner.nextLine();
-        System.out.print("Enter the shift value (1-25): ");
-        int shift = scanner.nextInt();
-        scanner.close();
+        for(char ch : msg.toCharArray()){
+            if(isUpper.test(ch)){
+                sb.append(((char)((ch - 'A' + shift) % 26 + 'A')));
+            }else if(isLower.test(ch)){
+                sb.append(((char)((ch - 'a' + shift) % 26 + 'a')));
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    };
+
+    private static final BiFunction<String,Integer,String> decrypt = (msg,shift) -> {
+        // In ceaser cyper we should shift each character to next kth character(here k = shift)
+
+        // Approach-i) Traditional way
+        // StringBuilder sb = new StringBuilder();
+
+        // for(char ch : msg.toCharArray()){
+        //     if(isUpper.test(ch)){
+        //         sb.append(((char)((ch - 'A' - shift + 26) % 26 + 'A')));
+        //     }else if(isLower.test(ch)){
+        //         sb.append(((char)((ch - 'a' - shift + 26) % 26 + 'a')));
+        //     }else{
+        //         sb.append(ch); // for spaces or punctunations
+        //     }
+        // }
+        // return sb.toString();
+
+        // Approach-ii) Call encyrpt with shift = 26 - shift
+        return encrypt.apply(msg, 26 - shift);
+    };
+
+
+
+    public static void main(String... caesarCipher) {
+        String msg;
+        int shift;
+
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter the message: ");
+            msg = sc.nextLine();
+
+            System.out.print("Enter the shift value (1-25): ");
+            shift = sc.nextInt();
+        }
+        
         if (shift < 1 || shift > 25) {
             System.out.println("Invalid shift value. Please enter a number between 1 and 25.");
             return;
         }
-        String encryptedMessage = encrypt(message, shift);
+
+        String encryptedMessage = encrypt.apply(msg, shift);
         System.out.println("Encrypted Message: " + encryptedMessage);
-        String decryptedMessage = decrypt(encryptedMessage, shift);
+
+        String decryptedMessage = decrypt.apply(encryptedMessage, shift);
         System.out.println("Decrypted Message: " + decryptedMessage);
     }
 }
