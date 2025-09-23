7be38c67f08ca9fd2fe195b1d309d91c90accd08
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-09-23 22:49:55 +0530

    Updated with for-each loop

--- lab-internal-1/p1_XORWithZero.java
+++ lab-internal-1/p1_XORWithZero.java
@@ -1,6 +1,7 @@
-// 1.Write a Java program that contains a string (char pointer) with a value 'Hello World'. The 
-// program should XOR each character in this string with 0 and displays the result. 
-// Program: 
+/* 1.Write a Java program that contains a string (char pointer) with a value 'Hello World'. The 
+program should XOR each character in this string with 0 and displays the result. 
+ */
+ 
 
 public class p1_XORWithZero {
 
@@ -9,11 +10,9 @@ public class p1_XORWithZero {
         System.out.println("Original String: " + text);
         
         System.out.print("XOR with 0 result: ");
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            char xorResult = (char) (c ^ 0);
-            System.out.print(xorResult);
+
+        for(char ch : text.toCharArray()){
+            System.out.print((char)(ch ^ 0));
         }
-        System.out.println();
     }
 }
