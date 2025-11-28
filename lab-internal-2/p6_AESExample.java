012805c5940d458deb0a2756ffc16ed14d24be16
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-11-28 23:08:45 +0530

    Refactored AES

--- lab-internal-2/p6_AESExample.java
+++ lab-internal-2/p6_AESExample.java
@@ -8,42 +8,47 @@ import javax.crypto.SecretKey;
 public class p6_AESExample {
     
     // Method to generate a secret key 
-    
-    public static SecretKey generateKey(int n) throws Exception {
+    private SecretKey generateKey(int n) throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         keyGenerator.init(n);
         return keyGenerator.generateKey();
     }
     
     // Method to encrypt data using the AES algorithm 
-    public static String encrypt(String plaintext, SecretKey key) throws Exception {
+    private String encrypt(String plaintext, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes()); // convert to bytes 
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
     
     // Method to decrypt data using the AES algorithm 
-    public static String decrypt(String ciphertext, SecretKey key) throws Exception {
+    private String decrypt(String ciphertext, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(ciphertext));
         return new String(decryptedBytes);
     }
 
-    public static void main(String[] args) {
+    public static void main(String... helloAES) {
+        p6_AESExample obj = new p6_AESExample(); 
+
         try {
             // Generate a secret key for AES 
-            SecretKey secretKey = generateKey(128);
+            SecretKey secretKey = obj.generateKey(128);
+            
             // Plain text to be encrypted 
-            String plaintext = "Hello, World!";
+            String plaintext = "Kash , hamara sapne purre hoga! __**__";
             System.out.println("Original Text: " + plaintext);
+            
             // Encrypt the plain text 
-            String encryptedText = encrypt(plaintext, secretKey);
+            String encryptedText = obj.encrypt(plaintext, secretKey);
             System.out.println("Encrypted Text: " + encryptedText);
+            
             // Decrypt the encrypted text 
-            String decryptedText = decrypt(encryptedText, secretKey);
+            String decryptedText = obj.decrypt(encryptedText, secretKey);
             System.out.println("Decrypted Text: " + decryptedText);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
